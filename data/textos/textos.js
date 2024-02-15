471a4d94ff1eaac3a390fce94d9f8030b6420e3a
mcarmegimenez <160177314+mcarmegimenez@users.noreply.github.com>
2024-02-15 23:06:00 +0100

    Add files via upload

---
+++ data/textos/textos.js
@@ -0,0 +1,102 @@
+textos = {
+
+inicio: '<p>Assaig de materials</p>',
+
+rotulo_final: '<p>Posa’t a prova</p>',
+
+bocadillo_0: '<p>Hola, sóc un robot molt curiós i vull experimentar les propietats dels materials. M’ajudes, humà?</p>',
+
+empezar: '<p>Continua</p>',
+
+robot_escena_1: '<p>Arrossega el got a les 4 proves.<br>A veure com es comporta.</p>',
+
+texto_linterna_vaso: '<p>És transparent</p>',
+
+texto_punzon_vaso: '<p>No es ratlla, és dur</p>',
+
+texto_circuito_vaso: '<p>No és conductor</p>',
+
+texto_martillo_vaso: '<p>És fràgil</p>',
+
+texto_linterna_tupper_tranparente: '<p>És transparent</p>',
+
+texto_linterna_tupper_opaco: '<p>És opac</p>',
+
+texto_punzon_tupper: '<p>Es ratlla</p>',
+
+texto_circuito_tupper: '<p>No és conductor</p>',
+
+texto_martillo_tupper: '<p>No es trenca</p>',
+
+texto_linterna_plato: '<p>És opac</p>',
+
+texto_punzon_plato: '<p>Es ratlla poc, és prou dur</p>',
+
+texto_circuito_plato: '<p>És conductor</p>',
+
+texto_martillo_plato: '<p>No es trenca</p>',
+
+texto_linterna_tabla: '<p>És opaca</p>',
+
+texto_punzon_tabla: '<p>Es ratlla</p>',
+
+texto_circuito_tabla: '<p>No és conductora</p>',
+
+texto_martillo_tabla: '<p>No es trenca</p>',
+
+robot_pulsa: '<p>A veure si has estat atent i predius el que passarà a aquests quatre objectes! Marca sí o no.</p>',
+
+corregir: '<p>Corregeix</p>',
+
+tabla_again: '<p>Torna a intentar-ho</p>',
+
+inicio_again: '<p>Des del principi</p>',
+
+si: '<p>SÍ</p>',
+
+no: '<p>NO</p>',
+
+matraz: '<p>Matràs</p>',
+
+caja_madera: '<p>Ceràmica</p>',
+
+regla: '<p>Regle</p>',
+
+contrachapado: '<p>Tauler fusta</p>',
+
+tiza: '<p>Clarió</p>',
+
+euro: '<p>Euro</p>',
+
+boli: '<p>Goma d’esborrar</p>',
+
+llave: '<p>Clau</p>',
+
+opaco: '<p>OPAC</p>',
+
+conductor: '<p>CONDUCTOR</p>',
+
+duro: '<p>DUR</p>',
+
+fragil: '<p>FRÀGIL</p>',
+
+enhorabuena: '<p>Enhorabona humà estàs fet un tecnorobot, com jo!',
+
+prueba_tupper: '<p>Posa a prova el recipient de plàstic.</p>',
+
+prueba_plato: '<p>Ara ens toca el plat metàl·lic.</p>',
+
+prueba_tabla: '<p>Prova la fusta.<br>A veure com es comporta.</p>',
+
+vidrio: '<p>vidre</p>',
+
+plastico: '<p>plàstic</p>',
+
+metal: '<p>metall</p>',
+
+madera: '<p>fusta</p>',
+
+
+
+}
+
